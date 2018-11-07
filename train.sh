54b4baf779bfc255f4b3a2d7c26c77c3b36f7635
fanyang <314043711@qq.com>
2018-11-07 16:34:02 +0800

    change option

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 main.py --env_name "carla" --experiment-desc "carla_0_3_5_without/" \
-    --save-model-dir "throttle=0.35, without randomization, with origin image, without smooth penalty" --max-training-num 1000000
+python3 main.py --env_name "carla" --experiment-desc "throttle=0.35, without randomization, with origin image, without smooth penalty" \
+    --save-model-dir "carla_0_3_5_without/" --num-steps 20 --max-training-num 1000000
