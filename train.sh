440951bceff1cae171fa3adac6e578530fba48c8
fanyang <314043711@qq.com>
2018-11-07 18:07:54 +0800

    fix bugs

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 main.py --env_name "carla" --experiment-desc "throttle=0.35, without randomization, with origin image, without smooth penalty" \
+python3 main.py  --experiment-desc "throttle=0.35, without randomization, with origin image, without smooth penalty" \
     --save-model-dir "carla_0_3_5_without/" --num-steps 20 --max-training-num 1000000
