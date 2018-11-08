26972f947160893774080803184ce77aae16641d
fanyang <314043711@qq.com>
2018-11-08 15:40:28 +0800

    ..

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 main.py  --experiment-desc "throttle=0.35, without randomization, with origin image, without smooth penalty" \
+python3 main.py  --experiment-desc "throttle=0.35, with start randomization, with origin image, with smooth penalty" \
     --save-model-dir "carla_0_3_5_without/" --num-processes 3 --num-steps 20 --max-training-num 8000000
